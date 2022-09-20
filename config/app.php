e5b5f9b85b0da5a455a72d64b89cdac2ef62ad59
Wahyu Kristiawan <whykris97@gmail.com>
2022-09-20 11:08:39 +0700

    swagger-ui

--- config/app.php
+++ config/app.php
@@ -174,7 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\SwaggerUiServiceProvider::class,
 
+        // JWT Auth
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ],
 
     /*
