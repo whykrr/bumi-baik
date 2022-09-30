ff398e452897e61d0f60203fb0fe0308b45e02a3
Wahyu Kristiawan <whykris97@gmail.com>
2022-10-01 01:37:00 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
     <script>
         // Set the date we're counting down to
-        var countDownDate = new Date("Sep 30, 2022 23:59:59").getTime();
+        var countDownDate = new Date("Oct 1, 2022 23:59:59").getTime();
 
         // Update the count down every 1 second
         var countdownfunction = setInterval(function() {
